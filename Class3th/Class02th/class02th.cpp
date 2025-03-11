a52d7713bf1087dd44da557c529d95ce36081c24
gyuchan1126 <125331250+gyuchan1126@users.noreply.github.com>
2025-03-11 18:22:16 +0900

    20250311 -1

---
+++ Class3th/Class02th/class02th.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int a = 3;
+	int b = 4;
+	int c = 5;
+	int d = 6;
+
+	printf("%d*%d=%d\n",a, b,a*b);
+
+	printf("%d / %d = %f", c, a,(float) c / a);
+
+
+
+	return 0;
+}
